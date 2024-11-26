80a180c7a2b599032c7d4464586ae708907a039b
김수현 <suhyun_113@naver.com>
2024-11-27 06:26:53 +0900

    implement hit_wall

--- implements.py
+++ implements.py
@@ -77,10 +77,13 @@ class Ball(Basic):
     def hit_wall(self):
         # ============================================
         # TODO: Implement a service that bounces off when the ball hits the wall
-        pass
-        # 좌우 벽 충돌
+        # 좌우 벽 충돌 => 반대방향으로 바꾸기
+        if self.rect.left <= 0 or self.rect.right >= config.display_dimension[0]:
+            self.dir = 180 - self.dir # 진행 방향 반대로 전환(좌우)
         
         # 상단 벽 충돌
+        if self.rect.top <= 0:
+            self.dir = 360 - self.dir # 진행 방향 반대로 전환(위 -> 아래)
     
     def alive(self):
         # ============================================
